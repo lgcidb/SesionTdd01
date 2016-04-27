ccbba275f746e713f51bb28041ff361a6c0415e7
lgcidb <lgcid@alumnos.ubiobio.cl>
2016-04-27 12:03:33 -0300

    Fallo primer test

--- src/test/java/cl/ubb/agil/CalculadoraTest.java
+++ src/test/java/cl/ubb/agil/CalculadoraTest.java
@@ -1,5 +1,22 @@
 package cl.ubb.agil;
 
-public class CalculadoraTest {
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
+public class CalculadoraTest {
+	
+	@Test
+	public void SumarDosMasTresEsCinco(){
+		
+		/*Arrange*/
+		Calculadora calculadora = new Calculadora();
+		
+		/*Act*/
+		int result = calculadora.sumar(2, 3);
+		
+		/*Assert*/
+		assertThat(result,is(5));
+		
+	}
 }
